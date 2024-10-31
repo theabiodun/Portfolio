b9129308c12bb1575e1ea9d0e8317b0dd5c18ecd
Abiodun Adedoyin <abiodunadedoyin6@gmail.com>
2024-10-31 01:49:33 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,19 @@
 		});
 
 })(jQuery);
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeElems = document.querySelectorAll('.fade-in');
+  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  });
+
+  fadeElems.forEach(elem => observer.observe(elem));
+});
